7277f418cd99c8a71b2c9cb5ebc0f8f30ca320b8
FatimaMuhummad90 <fatimamuhummad648@gmail.com>
2025-05-21 05:20:38 +0500

    first change

---
+++ CityManagmentSystem.java
@@ -0,0 +1,18 @@
+
+abstract class CityResources{
+    protected String recourseID;
+    protected String location;
+    protected String Status;
+
+    
+}
+class TransportServices extends CityResources{
+
+}
+class EmergencyServices  extends CityResources{
+
+}
+
+public class CityManagmentSystem {
+
+}
